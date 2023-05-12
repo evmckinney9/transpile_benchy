307a998ea376290560b564bd5d264dc82ec763f3
Evan McKinney <evmckinney9@gmail.com>
2023-05-11 22:51:38 -0400

    setting up submodule file reading

--- src/transpile_benchy/main.py
+++ src/transpile_benchy/main.py
@@ -1,13 +1,27 @@
 """Main benchmark runner."""
+import glob
+import os
 
 
 class Benchmark:
     """Benchmark runner."""
 
-    def __init__():
+    def __init__(self):
         """Initialize benchmark runner."""
         pass
 
-    def run():
+    def run(self):
         """Run benchmark."""
-        pass
+        print(os.getcwd())
+        filenames = glob.iglob("../../QASMBench/small/**/*.qasm", recursive=True)
+        # # Filter out files containing '_transpiled' in their names
+        filtered_filenames = (
+            filename for filename in filenames if "_transpiled" not in filename
+        )
+
+        for filename in filtered_filenames:
+            print(filename)
+
+
+if __name__ == "__main__":
+    Benchmark().run()
