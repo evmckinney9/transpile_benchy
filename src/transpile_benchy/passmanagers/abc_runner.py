9cf7d48d24cdf769226ebf53f0d20b1d6055ef53
Evan McKinney <evmckinney9@gmail.com>
2023-07-12 18:39:05 -0400

    wip, runner stage refactor

--- src/transpile_benchy/passmanagers/abc_runner.py
+++ src/transpile_benchy/passmanagers/abc_runner.py
@@ -3,9 +3,7 @@
 It defines the pre_process(), main_process(), and post_process() methods
 which are meant to be implemented in subclasses for different types of
 transpilers. The run() method is also defined here which runs the three
-processing methods and returns the resulting circuit. An example class
-CustomPassManager is provided which is a custom transpiler that outlines
-the structure of the main processing method.
+processing methods and returns the resulting circuit.
 """
 from abc import ABC, abstractmethod
 
@@ -17,14 +15,18 @@ from transpile_benchy.metrics.abc_metrics import MetricInterface
 class CustomPassManager(ABC):
     """Abstract class outlining the structure of a custom PassManager."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, stages=None, **kwargs):
         """Initialize the runner."""
         self.name = None
         for key, value in kwargs.items():
             setattr(self, key, value)
         self.name = self.name or self.__class__.__name__
         self.property_set = {}
-        self.metric_passes = []
+        self.stages = stages if stages else []
+
+    def append_stage(self, stage_builder):
+        """Add a new stage to the sequence."""
+        self.stages.append(stage_builder)
 
     def append_metric_pass(self, metric: MetricInterface):
         """Append a analysis pass, using transpiler-specific configuration."""
@@ -33,6 +35,20 @@ class CustomPassManager(ABC):
     def build_metric_stage(self, **kwargs) -> PassManager:
         """Build a PassManager for metric analysis."""
         return PassManager(self.metric_passes)
+
+    def run(self, circuit):
+        """Run the transpiler on the circuit."""
+        self.property_set = {}  # reset property set
+        for stage_builder in self.stages:
+            stage = stage_builder()
+            stage.property_set = self.property_set
+            circuit = stage.run(circuit)
+            self.property_set.update(stage.property_set)
+        return circuit
+
+
+class ThreeStageRunner(CustomPassManager):
+    """Abstract class outlining the structure of a custom PassManager."""
 
     @abstractmethod
     def build_pre_stage(self) -> PassManager:
@@ -48,18 +64,3 @@ class CustomPassManager(ABC):
     def build_post_stage(self) -> PassManager:
         """Build the post-process PassManager."""
         return PassManager()
-
-    def run(self, circuit):
-        """Run the transpiler on the circuit."""
-        self.property_set = {}  # reset property set
-        stages = [
-            self.build_pre_stage(),
-            self.build_main_stage(),
-            self.build_post_stage(),
-            self.build_metric_stage(),
-        ]
-        for stage in stages:
-            stage.property_set = self.property_set
-            circuit = stage.run(circuit)
-            self.property_set.update(stage.property_set)
-        return circuit
--- src/transpile_benchy/passmanagers/qiskit_baseline.py
+++ src/transpile_benchy/passmanagers/qiskit_baseline.py
@@ -1,3 +1,1 @@
 """Qiskit Baseline Pass Manager."""
-# TODO
-# CustomPassManager calls Qiskit optimization level 3 in main
